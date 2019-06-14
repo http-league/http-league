32116bf79544f7f4bb533630d78e0144a2860a2a
AzStowe <aaron.z.stowe@gmail.com>
2019-06-14 15:22:41 -0500

    superuser create

--- httpleague/settings.py
+++ httpleague/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'main_app',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,8 +76,8 @@ WSGI_APPLICATION = 'httpleague.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'httpleague',
     }
 }
 
