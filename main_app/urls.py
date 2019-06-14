7f200b5772fde673df16b4632dbe1e0a56fe422d
AzStowe <aaron.z.stowe@gmail.com>
2019-06-14 16:28:55 -0500

    auth

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/signup', views.signup, name='signup'),
+    
 ]
