63f84d48aa2d68962340396fa48643d10ef8fbf9
AzStowe <aaron.z.stowe@gmail.com>
2019-06-14 16:15:53 -0500

    starting auth

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,26 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Site(models.Model):
+    url = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+    pub_date =  models.DateField('publish date')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # category = 
+    # style = 
+    # tech-stack = 
+
+    def __str__(self):
+        return self.name
+
+
+# models
+#submission, comment, photo, cetegory, style, tech-stack
+
+# class Submission(Site):
+#     statement = TextField(max_length=500)
+    
 
