71087e855ed84948001e0a9dfe4d9d1282dfab1f
AzStowe <aaron.z.stowe@gmail.com>
2019-06-17 11:01:37 -0500

    added a few basic urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('category/<int:category_id>/', views.category_detail, name='category'),
+    path('sites/<int:sites_id>/', views.sites_detail, name='detail'),
 ]
