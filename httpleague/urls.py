8693dce6f55b1c1c9d2c1eed1fbc9827d25966f9
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-06-17 17:37:55 -0500

    Add SCSS style and base CSS Style Sheet

--- httpleague/urls.py
+++ httpleague/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from main_app import views 
+from main_app import views
 
 urlpatterns = [
     path('', include('main_app.urls')),
