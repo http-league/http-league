7d9e4fb8e5489ff285564b0a4439e945f56753d6
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-06-17 15:55:00 -0500

    Add home, about, blog_index, and sites_detail view functions

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,17 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('blog/', views.blog, name='blog'),
+    path('blog/', views.blog_index, name='blog'),
     path('about/', views.about, name='about'),
     path('category/<int:category_id>/', views.category_detail, name='category'),
-    path('sites/<int:sites_id>/', views.sites_detail, name='detail'),
-    path(r'^(?P<username>.+)/$', views.profile, name='profile'),
-    # Sites
-    path('site/<int:site_id/create/', views.SiteCreate.as_view(), name='sites_create'),
-    path('site/<int:pk>/update/', views.SiteUpdate.as_view(), name='sites_update'),
-    path('site/<int:pk>/delete/', views.SiteDelete.as_view(), name='sites_delete'),
-    # Comments
-    path('comment/<int:comment_id/create/', views.CommentCreate.as_view(), name='comments_create'),
-    path('comment/<int:pk>/update/', views.CommentUpdate.as_view(), name='comments_update'),
-    path('comment/<int:pk>/delete/', views.CommentDelete.as_view(), name='comments_delete'),
+    path('sites/<int:site_id>/', views.site_detail, name='detail'),
+
+    # # Sites
+    # path('sites/<int:site_id/create/', views.SiteCreate.as_view(), name='sites_create'),
+    # path('sites/<int:pk>/update/', views.SiteUpdate.as_view(), name='sites_update'),
+    # path('sites/<int:pk>/delete/', views.SiteDelete.as_view(), name='sites_delete'),
+
+    # # Comments
+    # path('comment/<int:comment_id/create/', views.CommentCreate.as_view(), name='comments_create'),
+    # path('comment/<int:pk>/update/', views.CommentUpdate.as_view(), name='comments_update'),
+    # path('comment/<int:pk>/delete/', views.CommentDelete.as_view(), name='comments_delete'),
 ]
