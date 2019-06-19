4ed5525912c9765ebbfad1765774a03137b86797
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-06-19 16:10:00 -0500

    Update with proper urls

--- httpleague/settings.py
+++ httpleague/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -119,6 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
---
+++ staticfiles/js/menu.js
@@ -0,0 +1,16 @@
+const toggleTag = document.querySelector('a.toggle-nav');
+const mainTag = document.querySelector('main');
+const ul = document.querySelector('ul.navbar-nav');
+
+// When I click the toggle tag add a class of open to the main tag
+
+toggleTag.addEventListener('click', function () {
+    mainTag.classList.toggle('open');
+})
+
+
+ul.addEventListener('click', handleClick);
+
+function handleClick(e) {
+    console.log(e.target);
+}
