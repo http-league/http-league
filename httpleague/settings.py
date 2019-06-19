79d89ce53a1ac4ac8c546f25d4ac7e98a3370058
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-06-19 17:24:44 -0500

    Update settings to include Whitenoise

--- httpleague/settings.py
+++ httpleague/settings.py
@@ -126,8 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    'static',
+    os.path.join(BASE_DIR, "static")
 ]
 
 LOGIN_REDIRECT_URL = '/'
