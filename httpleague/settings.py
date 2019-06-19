80173ec83aa2fd3669dc0a0a268cdd5149771a13
AzStowe <aaron.z.stowe@gmail.com>
2019-06-19 17:24:08 -0500

    setting changes for heroku

--- httpleague/settings.py
+++ httpleague/settings.py
@@ -17,6 +17,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+print('base dir path', BASE_DIR)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -121,13 +123,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
+
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '../static/',
 ]
+
+
 
 LOGIN_REDIRECT_URL = '/'
 
