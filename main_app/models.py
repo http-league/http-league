021dd66e66f8631f6c4e0a4bf1b0a4097916ef0c
AzStowe <aaron.z.stowe@gmail.com>
2019-06-19 13:47:15 -0500

    Profile model added, and finished adding absolute urls

--- main_app/models.py
+++ main_app/models.py
@@ -6,7 +6,7 @@ from django.db.models.signals import post_save
 
 # Create your models here.
 
-class UserProfile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     description = models.CharField(max_length=100, default='')
     email = models.CharField(max_length=75, default='')
